74e71a101cb6f85a4d1aa0825f315ed8226fb8ad
Dadaso <Dadaso@gmail.com>
2021-10-10 21:15:26 +0530

    Added calculator logic

--- src/test/java/com/dada/Calculator.java
+++ src/test/java/com/dada/Calculator.java
@@ -1,9 +1,22 @@
 package com.dada;
 
+import java.util.Scanner;
+
 public class Calculator {
 
 	public static void main(String[] args) {
-		System.out.println("----WE are creating calculator----");
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter your first number");
+		int number1=sc.nextInt();
+		
+		System.out.println("Enter your second number number");
+		int number2=sc.nextInt();
+		
+		System.out.println("Addition of two number"+(number1+number2));
+		System.out.println("Substraction of two number"+(number1-number2));
+		System.out.println("Multiplication of two number"+(number1*number2));
+		System.out.println("Division of two number"+(number1/number2));
+		
 	}
 
 }
